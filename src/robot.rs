6b43604e5c57a59b09d15a44556a21fabb42617c
RabiyahMirAhmed <132773492+RabiyahMirAhmed@users.noreply.github.com>
2023-12-07 15:22:56 +0100

    Update robot.rs

--- src/robot.rs
+++ src/robot.rs
@@ -5,8 +5,53 @@ enum SendType {
     Sort,  // send sort info to robot
 }
 
+
 enum Receivetype {
-    OrderConfirmaton, // the order is done
+    OrderConfirmation, // the order is done
     SortConfirmation, // confirm I put into lager
     SortRequest,      // where to place
 }
+
+fn send_order(order_id: u8,positions:Vec<Position>){
+    let person_json = json!({
+        "command": SendType::Order,
+        "order id": order_id,
+        "positions": positions
+    });
+    send_and_receive_data("dd", person_json);
+}
+
+fn send_sort(positions:Position){
+    let person_json = json!({
+        "command": SendType::Sort,
+        "positions": position,
+    });
+    send_and_receive_data("dd", person_json);
+}
+
+
+fn receive_orderconfrimation(orderconfrimation, order_id: u8){
+    let person_json = json!({
+      "command": Receivetype::OrderConfirmation, 
+      "order_id": order_id,
+      "orderconfirmation": orderconfrimation,
+
+    });
+    
+
+}
+
+fn receive_sortconfrimation(sortconfirmation){
+  let person_json = json!({
+    "command": Receivetype::SortConfirmation,
+    "sortconformation": sortconformition,
+   });  
+}
+
+fn received_sortrequest(color:Color){
+    let oerson_json = json({
+        "command":Receivetype::SortRequest
+        "colors": color,
+    });
+}
+   
