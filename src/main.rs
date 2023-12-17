d9bfe6cd3571a93b03ab85a4dc1d343a14ebf6c8
ex@e.com <ex@e.com>
2023-12-17 19:21:37 +0100

    h

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@ use std::{
     io::{Read, Write},
     net::{SocketAddr, TcpStream},
     sync::{Arc, Mutex},
-    thread, time::Duration,
+    thread,
+    time::Duration,
 };
 
-use server::{Grid, read_order_updates, Position};
+use server::{read_order_updates, Grid, Position};
 
 // Importera nödvändiga bibliotek för serialisering och deserialiserinf av JSON
 
@@ -35,23 +36,23 @@ pub fn send_and_receive_data(ip: &str, data: &str) -> Result<String, std::io::Er
 }
 
 #[derive(Clone)]
-struct Queue<T: Clone> where T: Clone{
-    list_of_queue:Vec<T>,
+struct Queue<T: Clone>
+where
+    T: Clone,
+{
+    list_of_queue: VecDeque<T>,
 }
 
-fn read_database_thread(read_order_flag: Arc<Mutex<bool>>, orders:Arc<Mutex<Queue<([u16;4], u16)>>>){
+fn read_database_thread(orders: Arc<Mutex<Queue<([u16; 4], u16)>>>) {
     loop {
         {
-            let mut read_flag = read_order_flag.lock().expect("Failed to lock read_order_flag");
-
-            if *read_flag {
-                match server::read_order_updates() {
-                    Some(order) => {
-                        *read_flag = false;
-                        orders.lock().expect("Failed to lock current_orders").list_of_queue.push(order);
-                    }
-                    None => {}
-                }
+            match server::read_order_updates() {
+                Some(order) => orders
+                    .lock()
+                    .expect("Failed to lock current_orders")
+                    .list_of_queue
+                    .push_front(order),
+                None => {}
             }
         }
 
@@ -59,60 +60,83 @@ fn read_database_thread(read_order_flag: Arc<Mutex<bool>>, orders:Arc<Mutex<Queu
     }
 }
 
-fn read_robot_thread(){
-}
-
-type OrdersFinished = Arc<Mutex<Queue::<(Vec<Position>, u16)>>>;
-type CurrentOrders = Arc<Mutex<Queue::<([u16; 4], u16)>>>;
+type OrdersFinished = Arc<Mutex<Queue<(Vec<Position>, u16)>>>;
+type CurrentOrders = Arc<Mutex<Queue<([u16; 4], u16)>>>;
 
 fn main() {
     let grid = Arc::new(Mutex::new(Grid::new()));
 
-    let current_orders = CurrentOrders::new(Mutex::new(Queue { list_of_queue: vec![] }));
-    let finished_orders = OrdersFinished::new(Mutex::new(Queue { list_of_queue: vec![] }));
+    let orders_to_process = CurrentOrders::new(Mutex::new(Queue {
+        list_of_queue: VecDeque::new(),
+    }));
+    let finished_orders = OrdersFinished::new(Mutex::new(Queue {
+        list_of_queue: VecDeque::new(),
+    }));
 
-    let read_order_flag = Arc::new(Mutex::new(true));
+    let current_order: Arc<Mutex<Option<([u16; 4], u16)>>> = Arc::new(Mutex::new(None));
 
+    let is_order_in_process = Arc::new(Mutex::new(false));
 
-    let current_orders_1 = current_orders.clone();
-    let read_order_flag_1 = read_order_flag.clone();
+    let current_orders_1 = orders_to_process.clone();
 
     let finished_add = finished_orders.clone();
 
     thread::spawn(move || {
-       read_database_thread(read_order_flag_1, current_orders_1);
+        read_database_thread(current_orders_1);
     });
 
     thread::spawn(move || {
-        robot::robot_read(read_order_flag.clone(), finished_add.clone());
+        robot::robot_read(finished_add.clone());
     });
 
     loop {
         thread::sleep(Duration::from_secs(10));
-        process_order_queue(grid.clone(), current_orders.clone());
+        process_order_queue(
+            grid.clone(),
+            orders_to_process.clone(),
+            is_order_in_process.clone(),
+            current_order.clone(),
+        );
         process_finished_order(finished_orders.clone());
-      
     }
-   // server::send_order_done_db(vec![Position{position_x: 3, position_y: 3}], 9);
+    // server::send_order_done_db(vec![Position{position_x: 3, position_y: 3}], 9);
 }
 
-fn process_finished_order(finished_orders: OrdersFinished){
-    let mut orders_done = finished_orders.lock().unwrap();
-    if orders_done.list_of_queue.len() > 0{
-        let order_done = orders_done.list_of_queue.pop().unwrap();
+fn process_finished_order(finished_orders: OrdersFinished) {
+    let orders_done_len = finished_orders.lock().unwrap().list_of_queue.len();
+    if orders_done_len > 0 {
+        let order_done = finished_orders
+            .lock()
+            .unwrap()
+            .list_of_queue
+            .pop_front()
+            .unwrap();
+
         server::send_order_done_db(order_done.0, order_done.1 as u32);
     }
 }
 
-fn process_order_queue(grid:Arc<Mutex<Grid>>,current_orders:CurrentOrders){
-    let mut current_orders = current_orders.lock().expect("Failed to lock current_orders");
-    let order_queue = &mut current_orders.list_of_queue;
-
-    if order_queue.len() > 0 {
-        let g = grid.lock();
-
-       
-        let positions = g.unwrap().get_positions_for_order( order_queue[0].0);
-        robot::send_order(order_queue[0].1 as u8, positions);
+fn process_order_queue(
+    grid: Arc<Mutex<Grid>>,
+    orders_to_process: CurrentOrders,
+    is_order_process: Arc<Mutex<bool>>,
+    current_order: Arc<Mutex<Option<([u16; 4], u16)>>>,
+) {
+    let is_order_process_c = is_order_process.lock().unwrap().clone();
+    if !is_order_process_c {
+        let order_queue_len = orders_to_process.lock().unwrap().list_of_queue.len();
+        if order_queue_len > 0 {
+            let g = grid.lock();
+            *is_order_process.lock().unwrap() = true;
+            let order_to_send = orders_to_process
+                .lock()
+                .unwrap()
+                .list_of_queue
+                .pop_front()
+                .unwrap();
+            *current_order.lock().unwrap() = Some(order_to_send.clone());
+            let positions = g.unwrap().get_positions_for_order(order_to_send.0);
+            robot::send_order(order_to_send.1 as u8, positions);
+        }
     }
 }
--- src/robot/mod.rs
+++ src/robot/mod.rs
@@ -1,11 +1,21 @@
-use std::{net::{TcpListener, TcpStream}, sync::{Arc, Mutex}, time::Duration, io::Read, thread};
+use std::{
+    io::Read,
+    net::{TcpListener, TcpStream},
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
+};
 
-use serde_derive::{Serialize, Deserialize};
+use serde_derive::{Deserialize, Serialize};
 use serde_json::{json, Number, Value};
 
-use crate::{send_and_receive_data, server::{Position, self, Color}, Queue};
+use crate::{
+    send_and_receive_data,
+    server::{self, Color, Position},
+    Queue,
+};
 
-const ROBOT_IP:&str = "PLACE HOLDER";
+const ROBOT_IP: &str = "PLACE HOLDER";
 
 #[derive(Serialize, Deserialize)]
 enum SendType {
@@ -13,18 +23,17 @@ enum SendType {
     Sort,  // send sort info to robot
 }
 
-
 #[derive(Serialize, Deserialize)]
 #[repr(u8)]
 enum Receivetype {
     OrderConfirmation, // the order is done
-    SortConfirmation, // confirm I put into lager
-    SortRequest,      // where to place
+    SortConfirmation,  // confirm I put into lager
+    SortRequest,       // where to place
 }
 
 impl From<u8> for Receivetype {
     fn from(value: u8) -> Self {
-        match value{
+        match value {
             0 => Self::OrderConfirmation,
             1 => Self::SortConfirmation,
             2 => Self::SortRequest,
@@ -33,77 +42,72 @@ impl From<u8> for Receivetype {
     }
 }
 
-pub fn robot_read(order_read_flag: Arc<Mutex<bool>>, finished_orders: Arc<Mutex<Queue<(Vec<Position>, u16)>>>){
-
-
-    let stream =TcpStream::connect(ROBOT_IP).unwrap();
+pub fn robot_read(finished_orders: Arc<Mutex<Queue<(Vec<Position>, u16)>>>) {
+    let stream = TcpStream::connect(ROBOT_IP).unwrap();
     stream.set_read_timeout(Some(Duration::from_millis(500)));
 
     let mut stream_read = Arc::new(Mutex::new(stream));
 
     let mut stream_write = stream_read.clone();
 
-    
-    read_robot_commands(stream_read, order_read_flag);
+    read_robot_commands(stream_read, finished_orders);
 }
 
-fn write_robot(){
+fn write_robot() {}
 
-}
-
-fn read_robot_commands(stream: Arc<Mutex<TcpStream>>, order_read_flag: Arc<Mutex<bool>>){
+fn read_robot_commands(
+    stream: Arc<Mutex<TcpStream>>,
+    finished_orders: Arc<Mutex<Queue<(Vec<Position>, u16)>>>,
+) {
     loop {
         let mut buffer = String::new();
         {
-        let mut read_half = stream.lock().unwrap();
+            let mut read_half = stream.lock().unwrap();
 
             read_half.read_to_string(&mut buffer);
         }
 
-        let order_read_flag = order_read_flag.clone();
-
-        if buffer.len() > 0{
-        thread::spawn(move ||{
-            interpret_robot(buffer, order_read_flag.clone());
-        });
+        if buffer.len() > 0 {
+            let finished_orders = finished_orders.clone();
+            thread::spawn(move || {
+                interpret_robot(buffer, finished_orders);
+            });
+        }
     }
 }
-        
-}
 
+fn interpret_robot(buffer: String, finished_orders: Arc<Mutex<Queue<(Vec<Position>, u16)>>>) {
+    if buffer.len() > 0 {
+        let s: Value = serde_json::from_str(&buffer).unwrap();
 
-fn interpret_robot(buffer:String, order_read_flag: Arc<Mutex<bool>>){
-    if buffer.len() > 0{
-        let s:Value = serde_json::from_str(&buffer).unwrap();
+        let command_type: Receivetype = (s["command"].as_u64().unwrap() as u8).into();
 
-        let command_type:Receivetype = (s["command"].as_u64().unwrap() as u8).into();
-
-        match  command_type{
+        match command_type {
             Receivetype::OrderConfirmation => {
                 let order_id = s["order_id"].as_u64().unwrap();
-                let positions:Vec<Position> =serde_json::from_str(&s["positions"].to_string()).unwrap();
+                let positions: Vec<Position> =
+                    serde_json::from_str(&s["positions"].to_string()).unwrap();
 
-                
                 // TODO send confirm to database
-            },
+            }
             Receivetype::SortConfirmation => {
                 //TODO Update database
-            },
+            }
             Receivetype::SortRequest => todo!(),
         }
     }
 }
 
-pub fn send_order(order_id: u8,positions:Vec<Position>){
+pub fn send_order(order_id: u8, positions: Vec<Position>) {
     let person_json = json!({
-        "command": SendType::Order as u8,
+        "command": "order",
         "order id": order_id,
         "positions": positions
     });
     send_and_receive_data("dd", person_json.to_string().as_str());
 }
 
-fn send_sort(positions:Position, color:Color){ 
+fn send_sort(positions: Position, color: Color) {
     let person_json = json!({
         "command": SendType::Sort as u8,
         "color": color.to_str(),
@@ -112,27 +116,22 @@ fn send_sort(positions:Position, color:Color){
     send_and_receive_data("dd", person_json.as_str().unwrap());
 }
 
-
-
-fn receive_orderconfrimation(order_id: u8){
+fn receive_orderconfrimation(order_id: u8) {
     let person_json = json!({
-      "command": Receivetype::OrderConfirmation as u8, 
-      //"positions" vec<Positions> 
+      "command": Receivetype::OrderConfirmation as u8,
+      //"positions" vec<Positions>
       "order_id": order_id,
     });
-    
-
 }
 
-fn receive_sortconfrimation(){
-  let person_json = json!({
-    "command": Receivetype::SortConfirmation as u8,
-   });  
+fn receive_sortconfrimation() {
+    let person_json = json!({
+     "command": Receivetype::SortConfirmation as u8,
+    });
 }
 
-fn received_sortrequest(){
+fn received_sortrequest() {
     let oerson_json = json!({
         "command":Receivetype::SortRequest as u8
     });
 }
-   
